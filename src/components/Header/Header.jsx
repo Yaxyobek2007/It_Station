8f160bedc8f0d0e288278b24bd1dc698892f11b9
Asatillo-2005 <gsatoro517@gmail.com>
2023-08-14 21:31:04 +0500

    update new

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.scss"
+
+function Header(){
+    return(
+        <section className="section-header">
+            <div className="container">
+                <div className="header-box">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, distinctio!j
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Header
