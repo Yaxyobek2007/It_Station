2d06988dda9a5dce22df4106b2b5a862346325d7
zednegr <negrzed@gmail.com>
2023-08-15 19:14:51 +0500

    zed update

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -6,6 +6,12 @@ import "./experience.scss"
 
 import ExperiencePython from "../../assets/png/experience-python.png"
 
+// Import Swiper Component and Styles
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 function Experience() {
 
@@ -14,30 +20,98 @@ function Experience() {
             <div className="container">
 
                 <div className="experience-start-title">
-                    <h3 className="experience-start-h3">Bizning oquvchilar tajribasi</h3>    
+                    <h3 className="experience-start-h3">Bizning oquvchilar tajribasi</h3>
                 </div>
 
                 <div className="experience-wrapper">
-                    <div className="experience-card">
 
-                        <div className="experience-card-right">
-                            <img className="experience-card-right-img" src={ExperiencePython} alt="Network Error" />
-
-                            <div className="experience-price">
-                                <h5 className="experience-price-h5">$ 380</h5>
-                                <h5 className="experience-price-line-h5">$ 500</h5>
-                            </div>
-                        </div>
-
-                        <div className="experience-card-left">
-
-                            <p className="experience-date-p">1 - 28 July 2022</p>
-                            <h3 className="experience-card-name-h3">Product Management Basic - Course</h3>
-                            <p className="experience-description-p">Product Management Masterclass, you will learn with Sarah Johnson - Head of Product Customer Platform Gojek Indonesia.</p>
+                    <Swiper
+                         slidesPerView={1}
+                         spaceBetween={-150}
+                         pagination={{
+                           clickable: true,
+                         }}
                         
-                        </div>
+                            autoplay={{
+                              delay: 2500,
+                              disableOnInteraction: false,
+                            }}
+                            navigation={true}
+                            modules={[Autoplay, Pagination, Navigation]}
+                            className="mySwiper"
+                    >
 
-                    </div>
+                        <SwiperSlide>
+                            <div className="experience-card">
+
+                                <div className="experience-card-right">
+                                    <img className="experience-card-right-img" src={ExperiencePython} alt="Network Error" />
+
+                                    <div className="experience-price">
+                                        <h5 className="experience-price-h5">$ 380</h5>
+                                        <h5 className="experience-price-line-h5">$ 500</h5>
+                                    </div>
+                                </div>
+
+                                <div className="experience-card-left">
+
+                                    <p className="experience-date-p">1 - 28 July 2022</p>
+                                    <h3 className="experience-card-name-h3">Product Management Basic - Course</h3>
+                                    <p className="experience-description-p">Product Management Masterclass, you will learn with Sarah Johnson - Head of Product Customer Platform Gojek Indonesia.</p>
+
+                                </div>
+
+                            </div>
+                        </SwiperSlide>
+                        
+                        <SwiperSlide>
+                            <div className="experience-card">
+
+                                <div className="experience-card-right">
+                                    <img className="experience-card-right-img" src={ExperiencePython} alt="Network Error" />
+
+                                    <div className="experience-price">
+                                        <h5 className="experience-price-h5">$ 380</h5>
+                                        <h5 className="experience-price-line-h5">$ 500</h5>
+                                    </div>
+                                </div>
+
+                                <div className="experience-card-left">
+
+                                    <p className="experience-date-p">1 - 28 July 2022</p>
+                                    <h3 className="experience-card-name-h3">Product Management Basic - Course</h3>
+                                    <p className="experience-description-p">Product Management Masterclass, you will learn with Sarah Johnson - Head of Product Customer Platform Gojek Indonesia.</p>
+
+                                </div>
+
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="experience-card">
+
+                                <div className="experience-card-right">
+                                    <img className="experience-card-right-img" src={ExperiencePython} alt="Network Error" />
+
+                                    <div className="experience-price">
+                                        <h5 className="experience-price-h5">$ 380</h5>
+                                        <h5 className="experience-price-line-h5">$ 500</h5>
+                                    </div>
+                                </div>
+
+                                <div className="experience-card-left">
+
+                                    <p className="experience-date-p">1 - 28 July 2022</p>
+                                    <h3 className="experience-card-name-h3">Product Management Basic - Course</h3>
+                                    <p className="experience-description-p">Product Management Masterclass, you will learn with Sarah Johnson - Head of Product Customer Platform Gojek Indonesia.</p>
+
+                                </div>
+
+                            </div>
+                        </SwiperSlide>
+
+                    </Swiper>
+
                 </div>
             </div>
 
