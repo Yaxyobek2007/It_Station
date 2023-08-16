9dc64b8775bceee816ec7bbcbde214fda890e305
zednegr <negrzed@gmail.com>
2023-08-16 09:56:05 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,14 @@ import Experience from './components/Experience/Experience'
 import Header from './components/Header/Header.jsx'
 import Teachers from './components/teachers/teachers'
 import Fields from './components/Fields/Fields'
+import Hero from './components/Hero/Hero'
 
 function App() {
 
   return (
     <>
       <Header />
-<<<<<<< HEAD
-      <Fields />
-=======
-      {/* <Hero/> */}
->>>>>>> d76e1ed76b75eb125f6d168b7dd62e4f73539dde
+      <Hero />
       <Teachers />
       <Course />
       <Experience />
