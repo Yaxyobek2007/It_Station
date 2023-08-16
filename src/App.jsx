31235b11fb01ccdd405d3fcb30a88d32dd2f072b
Asatillo-2005 <gsatoro517@gmail.com>
2023-08-16 09:30:12 +0500

    push new

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Course from './components/Course/Course'
 import Experience from './components/Experience/Experience'
 import Header from './components/Header/Header.jsx'
 import Teachers from './components/teachers/teachers'
+import Hero from "./components/Hero/Hero.jsx"
 
 function App() {
 
