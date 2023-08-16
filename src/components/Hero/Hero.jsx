6ad54f7c6a1a1b1775d79d119d963439b10a6611
Asatillo-2005 <gsatoro517@gmail.com>
2023-08-16 18:45:48 +0500

    updae

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import "./Hero.scss"
-import { useState, useEffect, useRef , memo} from "react"
+import { useState, useEffect, useRef, memo } from "react"
 
 import hero1 from "../../assets/svg/header-links2-vector.svg"
 import hero2 from "../../assets/svg/header-links2-vector2.svg"
@@ -22,42 +22,42 @@ function Hero() {
     const elPython = useRef()
     const eldf = useRef()
 
-    function onImg() {  
-        setTimeout(()=>{
+    function onImg() {
+        setTimeout(() => {
             elImg1.current.classList.add('.open-img')
             elImg1.current.style.width = "709px"
             elImg1.current.style.left = "0"
         })
-            setTimeout(() => {
-                elImg2.current.style.width = "562px"
-                elImg2.current.style.height = "352px"
-                elImg2.current.style.top = "41%"
-                elImg2.current.style.left = "70px"
-            }, 300)
-           setTimeout(() => {
-                elReact.current.style.top = "25%"
-                elReact.current.style.left = "80px"
-            }, 1000) 
-           setTimeout(() => {
-                elHtml.current.style.top = "28%"
-                elHtml.current.style.left = "134px"
-            }, 1000) 
-           setTimeout(() => {
-                elFigma.current.style.top = "29%"
-                elFigma.current.style.left = "460px"
-            }, 1000) 
-           setTimeout(() => {
-                elJs.current.style.top = "85%"
-                elJs.current.style.right = "-160px"
-            }, 1000) 
-           setTimeout(() => {
-                elPython.current.style.top = "113%"
-                elPython.current.style.left = "525px"
-            }, 1000) 
+        setTimeout(() => {
+            elImg2.current.style.width = "562px"
+            elImg2.current.style.height = "352px"
+            elImg2.current.style.top = "41%"
+            elImg2.current.style.left = "70px"
+        }, 300)
+        setTimeout(() => {
+            elReact.current.style.top = "25%"
+            elReact.current.style.left = "80px"
+        }, 200)
+        setTimeout(() => {
+            elHtml.current.style.top = "28%"
+            elHtml.current.style.left = "134px"
+        }, 200)
+        setTimeout(() => {
+            elFigma.current.style.top = "29%"
+            elFigma.current.style.left = "460px"
+        }, 200)
+        setTimeout(() => {
+            elJs.current.style.top = "85%"
+            elJs.current.style.right = "-160px"
+        }, 200)
+        setTimeout(() => {
+            elPython.current.style.top = "113%"
+            elPython.current.style.left = "525px"
+        }, 200)
     }
     useEffect(() => {
         onImg()
-    },[])
+    }, [])
 
     return (
         <section className="section-hero">
@@ -71,14 +71,14 @@ function Hero() {
                         </li>
 
                         <li className="hero-link-links2" ref={eldf}>
-                                <img className="hero-link-links2-img3" src={hero3} alt="error" ref={elReact} />
-                                <img className="hero-link-links2-html" src={hero6} alt="error" ref={elHtml} />
-                                <img className="hero-link-links2-img1" src={hero2} alt="error" ref={elImg1} />
-                                <img className="hero-link-links2-img2" src={hero1} alt="error" ref={elImg2} />
-                                <img className="hero-link-links2-figma" src={hero7} alt="error" ref={elFigma} />
-                                <img className="hero-link-links2-js" src={hero5} alt="error" ref={elJs} />
-                                <img className="hero-link-links2-python" src={hero4} alt="error" ref={elPython} />
-                                <img className="hero-link-links2-t" src={hero8} alt="error" />
+                            <img className="hero-link-links2-img3" src={hero3} alt="error" ref={elReact} />
+                            <img className="hero-link-links2-html" src={hero6} alt="error" ref={elHtml} />
+                            <img className="hero-link-links2-img1" src={hero2} alt="error" ref={elImg1} />
+                            <img className="hero-link-links2-img2" src={hero1} alt="error" ref={elImg2} />
+                            <img className="hero-link-links2-figma" src={hero7} alt="error" ref={elFigma} />
+                            <img className="hero-link-links2-js" src={hero5} alt="error" ref={elJs} />
+                            <img className="hero-link-links2-python" src={hero4} alt="error" ref={elPython} />
+                            <img className="hero-link-links2-t" src={hero8} alt="error" />
                         </li>
                     </ul>
                 </div>
